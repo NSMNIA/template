bab036fb0accf7adbe41056633b52e5c221bb669
NSMNIA <kevin.stoop@revolveddesign.com>
2025-06-14 20:31:30 +0200

    feat(storybook): add a11y addon
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -17,6 +17,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-themes'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
